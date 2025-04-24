1f347ab7dfbb4c94af4fc9ee28e37105d55d5244
nonecoding <bitowork1@outlook.com>
2025-04-24 19:22:11 +0800

    cover remote repository，push new version

---
+++ src/main/java/org/ai/aicopilotforapi/AiCopilotForApiApplication.java
@@ -0,0 +1,13 @@
+package org.ai.aicopilotforapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCopilotForApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCopilotForApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ai/aicopilotforapi/AiCopilotForApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ai.aicopilotforapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCopilotForApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
